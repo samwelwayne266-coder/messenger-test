3ad2047b5756d96bf4572a161c8b27e725b963eb
Wayneee <samwelwayne266@gmail.com>
2025-12-30 22:55:21 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,12 +11,10 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image || '/icon.png' 
+    icon: '/icon.png' 
   };
-
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
